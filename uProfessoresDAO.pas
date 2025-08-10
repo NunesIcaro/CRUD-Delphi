95ef36253d229a4d594663d26862d818365b17b1
NunesIcaro <icaronunes999@gmail.com>
2025-08-10 19:15:06 -0300

    DAO dos professores com EDITAR E EXCLUIR

--- uProfessoresDAO.pas
+++ uProfessoresDAO.pas
@@ -4,7 +4,9 @@ uses uProfessores, FireDAC.Comp.Client, System.SysUtils,FireDAC.Stan.Param;
 
 type TProfessoresDAO = class
   public
-  class procedure Inserir(const aProfessores: TProfessores; const aConnection: TFDConnection);
+  class procedure Adicionar(const aProfessores: TProfessores; const aConnection: TFDConnection);
+  class procedure Editar(const aProfessores: TProfessores; const aConnection: TFDConnection);
+  class procedure Excluir(const aProfessores: TProfessores; const aConnection: TFDConnection);
 end;
 
 
@@ -13,7 +15,43 @@ implementation
 
 { TProfessoresDAO }
 
-class procedure TProfessoresDAO.Inserir(const aProfessores: TProfessores;
+class procedure TProfessoresDAO.Editar(const aProfessores: TProfessores;
+  const aConnection: TFDConnection);
+var Query: TFDQuery;
+begin
+  Query := TFDQuery.Create(nil);
+
+  try
+    Query.Connection := aConnection;
+    Query.SQL.Text := 'UPDATE professores SET prof_nome = :Nome, prof_cpf = :CPF WHERE prof_codigo = :Codigo';
+    Query.ParamByName('Nome').AsString := aProfessores.pNome;
+    Query.ParamByName('CPF').AsString := aProfessores.pCPF;
+    Query.ParamByName('Codigo').AsInteger := aProfessores.IDProfessores;
+
+    Query.ExecSQL;
+  finally
+    Query.Free;
+  end;
+
+end;
+
+class procedure TProfessoresDAO.Excluir(const aProfessores: TProfessores;
+  const aConnection: TFDConnection);
+var Query: TFDQuery;
+begin
+  Query := TFDQuery.Create(nil);
+  try
+    Query.Connection := aConnection;
+    Query.SQL.Text := 'DELETE FROM professores WHERE prof_codigo = :Codigo';
+    Query.ParamByName('Codigo').AsInteger := aProfessores.IDProfessores;
+    Query.ExecSQL;
+  finally
+    Query.Free;
+  end;
+
+end;
+
+class procedure TProfessoresDAO.Adicionar(const aProfessores: TProfessores;
   const aConnection: TFDConnection);
 var Query: TFDQuery;
 begin
