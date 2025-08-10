949d14ffd7e283440f4ef59b8fb97f06213cd9ec
NunesIcaro <icaronunes999@gmail.com>
2025-08-10 19:02:55 -0300

    Comandos de Editar e Excluir do banco feitos

--- uEstudantesDAO.pas
+++ uEstudantesDAO.pas
@@ -7,29 +7,63 @@ uses uEstudantes, FireDac.Comp.Client, System.SysUtils,FireDAC.Stan.Param;
 type TEstudantesDAO = class
 
   public
-  class procedure Inserir(const aEstudantes: TEstudantes; const aConnection: TFDConnection);
+  class procedure Adicionar(const aEstudantes: TEstudantes; const aConnection: TFDConnection);
+  class procedure Editar(const aEstudantes: TEstudantes; const aConnection: TFDConnection);
+  class procedure Excluir(const aEstudantes: TEstudantes; const aConnection: TFDConnection);
 
 end;
 
 implementation
+{ TEstudantesDAO }
 
-{ TInformacoesDAO }
-
-class procedure TEstudantesDAO.Inserir(const aEstudantes: TEstudantes;
+//Procedure de Editar
+class procedure TEstudantesDAO.Editar(const aEstudantes: TEstudantes;
   const aConnection: TFDConnection);
-var Query : TFDQuery;
+var Query: TFDQuery;
 begin
-  if not Assigned(aEstudantes) then
-  raise Exception.Create('Estudante não pode ser nil');
+  Query := TFDQuery.Create(nil);
 
-  if not Assigned(aConnection) then
-    raise Exception.Create('Conexão inválida');
+  try
+    Query.Connection := aConnection;
+    Query.SQL.Text := 'UPDATE estudantes SET estu_nome = :Nome, estu_cpf = :CPF, estu_turmas_codigo = :Turma WHERE estu_codigo = :Codigo';
+    Query.ParamByName('Nome').AsString := aEstudantes.pNome;
+    Query.ParamByName('CPF').AsString := aEstudantes.pCPF;
+    Query.ParamByName('Turma').AsString := aEstudantes.Turma;
+    Query.ParamByName('Codigo').AsInteger := aEstudantes.IDEstudantes;
 
+    Query.ExecSQL;
+  finally
+    Query.Free;
+  end;
+
+end;
+
+//Procedure de Excluir
+class procedure TEstudantesDAO.Excluir(const aEstudantes: TEstudantes;
+  const aConnection: TFDConnection);
+var Query: TFDQuery;
+begin
   Query := TFDQuery.Create(nil);
   try
     Query.Connection := aConnection;
-    Query.SQL.Text := 'INSERT INTO estudantes (estu_codigo, estu_nome, estu_cpf, estu_turmas_codigo) VALUES (:IDEstudantes, :Nome, :CPF, :Turma)';
-    Query.ParamByName('IDEstudantes').AsInteger := aEstudantes.IDEstudantes;
+    Query.SQL.Text := 'DELETE FROM estudantes WHERE estu_codigo = :Codigo';
+    Query.ParamByName('Codigo').AsInteger := aEstudantes.IDEstudantes;
+    Query.ExecSQL;
+  finally
+    Query.Free;
+  end;
+
+end;
+
+//Procedure de Inserir
+class procedure TEstudantesDAO.Adicionar(const aEstudantes: TEstudantes;
+  const aConnection: TFDConnection);
+var Query : TFDQuery;
+begin
+  Query := TFDQuery.Create(nil);
+  try
+    Query.Connection := aConnection;
+    Query.SQL.Text := 'INSERT INTO estudantes (estu_codigo, estu_nome, estu_cpf, estu_turmas_codigo) VALUES (nextval(''Seq_estu_codigo''), :Nome, :CPF, :Turma)';
     Query.ParamByName('Nome').AsString := aEstudantes.pNome;
     Query.ParamByName('CPF').AsString := aEstudantes.pCPF;
     Query.ParamByName('Turma').AsString := aEstudantes.Turma;
