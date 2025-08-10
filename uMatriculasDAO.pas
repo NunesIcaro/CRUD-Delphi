810e1a72b4eb5a7b6d3481775cecca4e1e67a535
NunesIcaro <icaronunes999@gmail.com>
2025-08-10 19:34:24 -0300

    MatriculasDAO com Editar e Excluir

--- uMatriculasDAO.pas
+++ uMatriculasDAO.pas
@@ -8,8 +8,9 @@ type
   TMatriculasDAO = class
 
   public
-    class procedure Inserir(const aMatriculas: TMatriculas;
-      const aConnection: TFDConnection);
+    class procedure Adicionar(const aMatriculas: TMatriculas; const aConnection: TFDConnection);
+    class procedure Editar(const aMatriculas: TMatriculas; const aConnection: TFDConnection);
+    class procedure Excluir(const aMatriculas: TMatriculas; const aConnection: TFDConnection);
 
   end;
 
@@ -17,7 +18,7 @@ implementation
 
 { TMatriculasDAO }
 
-class procedure TMatriculasDAO.Inserir(const aMatriculas: TMatriculas;
+class procedure TMatriculasDAO.Adicionar(const aMatriculas: TMatriculas;
   const aConnection: TFDConnection);
 var Query: TFDQuery;
 begin
@@ -28,6 +29,42 @@ begin
     Query.ParamByName('IDTurma').AsInteger := aMatriculas.pCodigoTurmaM;
     Query.ParamByName('IDEstudante').AsInteger := aMatriculas.pCodigoEstuM;
 
+    Query.ExecSQL;
+  finally
+    Query.Free;
+  end;
+
+end;
+
+class procedure TMatriculasDAO.Editar(const aMatriculas: TMatriculas;
+  const aConnection: TFDConnection);
+var Query: TFDQuery;
+begin
+  Query := TFDQuery.Create(nil);
+
+  try
+    Query.Connection := aConnection;
+    Query.SQL.Text := 'UPDATE matriculas SET matri_turma_codigo = :CodigoTurma, matri_estu_codigo = :CodigoEstudante WHERE matri_codigo = :Codigo';
+    Query.ParamByName('CodigoTurma').AsInteger := aMatriculas.pCodigoTurmaM;
+    Query.ParamByName('CodigoEstudante').AsInteger := aMatriculas.pCodigoEstuM;
+    Query.ParamByName('matri_codigo').AsInteger := aMatriculas.pCodigoMatri;
+
+    Query.ExecSQL;
+  finally
+    Query.Free;
+  end;
+
+end;
+
+class procedure TMatriculasDAO.Excluir(const aMatriculas: TMatriculas;
+  const aConnection: TFDConnection);
+var Query: TFDQuery;
+begin
+  Query := TFDQuery.Create(nil);
+  try
+    Query.Connection := aConnection;
+    Query.SQL.Text := 'DELETE FROM matriculas WHERE matri_codigo = :Codigo';
+    Query.ParamByName('matri_codigo').AsInteger := aMatriculas.pCodigoMatri;
     Query.ExecSQL;
   finally
     Query.Free;
